9be79c3e38857cd18e407085cfed742b91fd672f
Paullo Cesar - PC <paullopcti@gmail.com>
2024-02-26 20:16:57 -0800

    adjust api error

--- front-end/services/api.js
+++ front-end/services/api.js
@@ -1,87 +1,59 @@
-import axios from 'axios'
-import { parseCookies, setCookie } from 'nookies'
-import { signOut } from '../Contexts/AuthContext'
+import axios from 'axios';
+import { parseCookies, setCookie } from 'nookies';
+import { signOut } from '../Contexts/AuthContext';
 import { AuthTokenError } from './errors/AuthTokenError';
 
+const api = axios.create({
+  baseURL: 'http://localhost:3001',
+});
 
-let isRefreshing = false;
-let failedRequestsQueue = [];
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalConfig = error.config;
 
-export function setupAPIClient(ctx) {
-  let cookies = parseCookies(ctx)
-  const api = axios.create({
-    baseURL: 'http://localhost:3001',
-    headers: {
-      Authorization: `Bearer ${cookies['plataform-education.token']}`
-    }
-  })
-
-  api.interceptors.response.use(response => response,
-    (error) => {
-      if (error.response.status == 401) {
-        if (error.response.data?.code === 'token.expired') {
-          cookies = parseCookies(ctx)
-          const { 'plataform-education.refreshToken': refreshToken } = cookies
-          const originalConfig = error.config
-
-          if (!isRefreshing) {
-            isRefreshing = true
-            api.post('/refresh', {
-              refreshToken
-            }).then(response => {
-              const { token } = response.data
-
-
-              setCookie(ctx, 'plataform-education.token', token, {
-                maxAge: 60 * 60 * 24 * 30,
-                path: '/'
-              })
-              setCookie(ctx, 'plataform-education.refreshToken', response.data.refreshToken, {
-                maxAge: 60 * 60 * 24 * 30,
-                path: '/'
-              })
-
-              api.defaults.headers['Authorization'] = `Bearer ${token}`
-              failedRequestsQueue.forEach(request => request.onSuccess(token))
-              failedRequestsQueue = []
-            }).catch((err) => {
-              failedRequestsQueue.forEach(request => request.onFailure(err))
-              failedRequestsQueue = []
-              if (typeof window) {
-                signOut()
-              }
-
-            })
-
-
-              .finally(() => {
-                isRefreshing = false;
-              })
+    if (error.response.status === 401 && error.response.data?.code === 'token.expired') {
+      if (!isRefreshing) {
+        isRefreshing = true;
+        try {
+          const { 'plataform-education.refreshToken': refreshToken } = parseCookies();
+          const response = await api.post('/refresh', { refreshToken });
+          const { token } = response.data;
+      
+          setCookie(null, 'plataform-education.token', token, {
+            maxAge: 60 * 60 * 24 * 30,
+            path: '/',
+          });
+          setCookie(null, 'plataform-education.refreshToken', response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30,
+            path: '/',
+          });
+      
+          api.defaults.headers['Authorization'] = `Bearer ${token}`;
+      
+          failedRequestsQueue.forEach((request) => request.onSuccess(token));
+          failedRequestsQueue = [];
+        } catch (err) {
+          failedRequestsQueue.forEach((request) => request.onFailure(err));
+          failedRequestsQueue = [];
+          if (typeof window !== 'undefined') {
+            signOut();
           }
-
-          return new Promise((resolve, reject) => {
-            failedRequestsQueue.push({
-              onSuccess: (token) => {
-                originalConfig.headers['Authorization'] = `Bearer ${token}`
-                resolve(api(originalConfig))
-              },
-              onFailure: (err) => {
-                reject(err)
-              }
-            })
-          })
-        } else {
-
-          if (typeof window) {
-            signOut()
-          } else {
-            return Promise.reject(new AuthTokenError())
-          }
+        } finally {
+          isRefreshing = false;
         }
       }
+      
+    }
 
-      return Promise.reject(error)
-    })
+    if (typeof window !== 'undefined') {
+      signOut();
+    } else {
+      return Promise.reject(new AuthTokenError());
+    }
 
-  return api;
-}
+    return Promise.reject(error);
+  }
+);
+
+export default api;
